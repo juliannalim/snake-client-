0114bab8cbe965bac41b99ae921d30ce6d4a7d64
Julianna <julianna.j.lim@gmail.com>
2020-07-01 15:45:51 -0400

    made a connection

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on("data", data => {
+    //do smthing when i receive the data
+    console.log(data);
+  });
+
+  //conn.on()
+  return conn;
+}
+
+
+
+module.exports = {connect};
