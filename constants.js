29ce9260c78f9a00675c57b299bcea3ff8c14d4b
Julianna <julianna.j.lim@gmail.com>
2020-07-01 22:50:10 -0400

    fixed some bugs

--- constants.js
+++ constants.js
@@ -1,16 +1,16 @@
 const PORT = 50541;
 const HOST = "135.23.222.148";
-const name = 'Name: Julianna ;
+const name = 'Name: Julianna';
 const MOVE_UP_KEY = 'Move: up';
 const MOVE_DOWN_KEY = 'Move: down';
 const MOVE_RIGHT_KEY = 'Move: right';
 const MOVE_LEFT_KEY = 'Move: left';
 
 module.exports = {
-  const PORT,
-  const IP,
-  const MOVE_UP_KEY,
-  const MOVE_DOWN_KEY,
-  const MOVE_LEFT_KEY,
-  const MOVE_RIGHT_KEY
+  PORT,
+  HOST,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY
 }
--- input.js
+++ input.js
@@ -4,27 +4,33 @@ const setupInput = function (conn) {
   stdin.setEncoding('utf8');
   stdin.resume();
   connection = conn;
+  //stdin.write("Say: hello");
 
   const handleUserInput = function (input) {
     console.log(input);
     if (input === "w") {
       //console.log("up")
-      connection.write("Move: up")
     }
     else if (input === "a") {
       //console.log("left")
-      connection.write("Move: left")
+      connection.write("Move: left");
     }
     else if (input === "s") {
       //console.log("down")
-      connection.write("Move: down")
+      connection.write("Move: down");
     }
     else if (input === "d") {
       //console.log("right")
-      connection.write("Move: right")
+      connection.write("Move: right");
     }
     else if (input === "\u0003") {
-      process.exit()
+      process.exit();
+    }
+    else if (input === "q") {
+      connection.write("Say: IDK")
+    }
+    else if (input === "g") {
+      connection.write("Say: hi")
     }
   }
   stdin.on('data', handleUserInput);
