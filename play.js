42cad65068f43147696eb32398933e076b7cafc1
Julianna <julianna.j.lim@gmail.com>
2020-07-01 20:22:02 -0400

    export setupInput function

--- play.js
+++ play.js
@@ -17,22 +17,26 @@
 //   return conn;
 // }
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+//   stdin.on('data', handleUserInput);
+//   return stdin;
+// }
 
-const handleUserInput = function (input) {
-  console.log(input);
- if (input === "\u0003")
- process.exit() 
-}
+// const handleUserInput = function (input) {
+//   console.log(input);
+//  if (input === "\u0003")
+//  process.exit() 
+// }
+
+
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+console.log('Connecting ...');
+connect();
 
 setupInput();
-// const { connect } = require('./client');
-// console.log('Connecting ...');
-//connect();
