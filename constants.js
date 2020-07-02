42588eb5ec2a309659f8961fbe060936b9868fba
Julianna <julianna.j.lim@gmail.com>
2020-07-01 21:29:57 -0400

    contains constant values

---
+++ constants.js
@@ -0,0 +1,16 @@
+const PORT = 50541;
+const HOST = "135.23.222.148";
+const name = 'Name: Julianna ;
+const MOVE_UP_KEY = 'Move: up';
+const MOVE_DOWN_KEY = 'Move: down';
+const MOVE_RIGHT_KEY = 'Move: right';
+const MOVE_LEFT_KEY = 'Move: left';
+
+module.exports = {
+  const PORT,
+  const IP,
+  const MOVE_UP_KEY,
+  const MOVE_DOWN_KEY,
+  const MOVE_LEFT_KEY,
+  const MOVE_RIGHT_KEY
+}
