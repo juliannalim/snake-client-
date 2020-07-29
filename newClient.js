94ba6860410b6bf138becc154e767476194760f4
Julianna <julianna.j.lim@gmail.com>
2020-07-29 12:23:42 -0400

    export an object containing our connect function to newClient.js

---
+++ newClient.js
@@ -0,0 +1,23 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on("data", function (message) {
+    //do smthing when i receive the data
+    // prints you ded cuz you idled from the server.... 
+    console.log(message);
+  });
+
+  return conn;
+};
+
+
+module.exports = { connect };
