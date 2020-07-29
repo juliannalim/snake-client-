bbba368abb80d6b7415f61cb8e79a03b00647df6
Julianna <julianna.j.lim@gmail.com>
2020-07-29 14:10:06 -0400

    added setupInput

--- newPlay.js
+++ newPlay.js
@@ -1,21 +1,7 @@
 const net = require('net');
+const { setupInput } = require('./newInput');
 const { connect } = require('./newclient');
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
-
-const handleUserInput = (input) => {
-  // control C to exit 
-  if (input === "\u0003") {
-    process.exit()
-  }
-}
 console.log('Connecting ...');
 connect();
+setupInput();
