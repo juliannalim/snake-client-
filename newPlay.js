e2a3d4e9a72477025593601019aa7580e0ab7a53
Julianna <julianna.j.lim@gmail.com>
2020-07-29 13:31:57 -0400

    created control C to exit using stidin configuration code and handUserInput function

--- newPlay.js
+++ newPlay.js
@@ -1,5 +1,21 @@
 const net = require('net');
 const { connect } = require('./newclient');
 
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (input) => {
+  // control C to exit 
+  if (input === "\u0003") {
+    process.exit()
+  }
+}
 console.log('Connecting ...');
 connect();
